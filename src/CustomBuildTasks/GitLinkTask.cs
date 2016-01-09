caa9cad8944e57eb2a81d66e7fccd7f3a3e5d3f1
Cameron MacFarland <distantcam@gmail.com>
2016-01-09 19:04:51 +0800

    Only run gitlink on main project

--- src/CustomBuildTasks/GitLinkTask.cs
+++ src/CustomBuildTasks/GitLinkTask.cs
@@ -7,9 +7,12 @@ public class GitLinkTask : ToolTask
     [Required]
     public ITaskItem SolutionDirectory { get; set; }
 
+    [Required]
+    public string ProjectName { get; set; }
+
     protected override string ToolName => "GitLink.exe";
 
     protected override string GenerateFullPathToTool() => Path.Combine(Path.GetDirectoryName(GetType().Assembly.Location), "GitLink.exe");
 
-    protected override string GenerateCommandLineCommands() => SolutionDirectory.FullPath();
+    protected override string GenerateCommandLineCommands() => $"{SolutionDirectory.FullPath()} -include {ProjectName}";
 }
