1eb105a06459cfdc4b073d698b703ff9abb1ea65
Cameron MacFarland <distantcam@gmail.com>
2016-01-09 18:48:17 +0800

    Running GitLink proper

--- src/CustomBuildTasks/CreatePackage.cs
+++ src/CustomBuildTasks/CreatePackage.cs
@@ -4,106 +4,104 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Xml.Linq;
+using CustomBuildTasks;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 using NuGet;
 
-namespace CustomBuildTasks
+public class CreatePackage : Task
 {
-    public class CreatePackage : Task
+    class PropertyProvider : Dictionary<string, dynamic>, IPropertyProvider
     {
-        class PropertyProvider : Dictionary<string, dynamic>, IPropertyProvider
+        dynamic IPropertyProvider.GetPropertyValue(string propertyName)
         {
-            dynamic IPropertyProvider.GetPropertyValue(string propertyName)
-            {
-                dynamic value;
-                if (TryGetValue(propertyName, out value))
-                    return value;
+            dynamic value;
+            if (TryGetValue(propertyName, out value))
+                return value;
 
-                return null;
-            }
+            return null;
+        }
+    }
+
+    [Required]
+    public ITaskItem DestinationFolder { get; set; }
+
+    [Required]
+    public ITaskItem NuSpecFile { get; set; }
+
+    [Required]
+    public ITaskItem ReferenceLibrary { get; set; }
+
+    public ITaskItem PackagesConfig { get; set; }
+
+    public override bool Execute()
+    {
+        try
+        {
+            InnerExecute();
+        }
+        catch (System.ComponentModel.DataAnnotations.ValidationException vex)
+        {
+            foreach (var line in Regex.Split(vex.Message, Environment.NewLine))
+                Log.LogError(line);
+        }
+        catch (Exception ex)
+        {
+            Log.LogErrorFromException(ex, true, true, null);
         }
 
-        [Required]
-        public ITaskItem DestinationFolder { get; set; }
+        return !Log.HasLoggedErrors;
+    }
 
-        [Required]
-        public ITaskItem NuSpecFile { get; set; }
+    private void InnerExecute()
+    {
+        var path = DestinationFolder.FullPath();
+        Directory.CreateDirectory(path);
 
-        [Required]
-        public ITaskItem ReferenceLibrary { get; set; }
-
-        public ITaskItem PackagesConfig { get; set; }
-
-        public override bool Execute()
-        {
-            try
-            {
-                InnerExecute();
-            }
-            catch (System.ComponentModel.DataAnnotations.ValidationException vex)
-            {
-                foreach (var line in Regex.Split(vex.Message, Environment.NewLine))
-                    Log.LogError(line);
-            }
-            catch (Exception ex)
-            {
-                Log.LogErrorFromException(ex, true, true, null);
-            }
-
-            return !Log.HasLoggedErrors;
-        }
-
-        private void InnerExecute()
-        {
-            var path = DestinationFolder.FullPath();
-            Directory.CreateDirectory(path);
-
-            var propertyProvider = new PropertyProvider()
+        var propertyProvider = new PropertyProvider()
             {
                 { "version", GitVersionInformation.NuGetVersionV2 }
             };
 
-            var packageBuilder = new PackageBuilder();
+        var packageBuilder = new PackageBuilder();
 
-            using (var spec = File.OpenRead(NuSpecFile.FullPath()))
-            {
-                var manifest = Manifest.ReadFrom(spec, propertyProvider, false);
-                packageBuilder.Populate(manifest.Metadata);
-            }
+        using (var spec = File.OpenRead(NuSpecFile.FullPath()))
+        {
+            var manifest = Manifest.ReadFrom(spec, propertyProvider, false);
+            packageBuilder.Populate(manifest.Metadata);
+        }
 
-            packageBuilder.PopulateFiles("", new[] { new ManifestFile { Source = ReferenceLibrary.FullPath(), Target = "lib" } });
+        packageBuilder.PopulateFiles("", new[] { new ManifestFile { Source = ReferenceLibrary.FullPath(), Target = "lib" } });
 
-            var debug = Path.ChangeExtension(ReferenceLibrary.FullPath(), ".pdb");
-            if (File.Exists(debug))
-            {
-                packageBuilder.PopulateFiles("", new[] { new ManifestFile { Source = debug, Target = "lib" } });
-            }
+        var debug = Path.ChangeExtension(ReferenceLibrary.FullPath(), ".pdb");
+        if (File.Exists(debug))
+        {
+            packageBuilder.PopulateFiles("", new[] { new ManifestFile { Source = debug, Target = "lib" } });
+        }
 
-            if (File.Exists(PackagesConfig.FullPath()))
-            {
-                var dependencies = new List<PackageDependency>();
+        if (File.Exists(PackagesConfig.FullPath()))
+        {
+            var dependencies = new List<PackageDependency>();
 
-                var doc = XDocument.Load(PackagesConfig.FullPath());
+            var doc = XDocument.Load(PackagesConfig.FullPath());
 
-                var packages = doc.Descendants()
-                    .Where(x => x.Name == "package" && x.Attribute("developmentDependency")?.Value != "true")
-                    .Select(p => new { id = p.Attribute("id").Value, version = SemanticVersion.Parse(p.Attribute("version").Value) })
-                    .Select(p => new PackageDependency(p.id, new VersionSpec() { IsMinInclusive = true, MinVersion = p.version }));
+            var packages = doc.Descendants()
+                .Where(x => x.Name == "package" && x.Attribute("developmentDependency")?.Value != "true")
+                .Select(p => new { id = p.Attribute("id").Value, version = SemanticVersion.Parse(p.Attribute("version").Value) })
+                .Select(p => new PackageDependency(p.id, new VersionSpec() { IsMinInclusive = true, MinVersion = p.version }));
 
-                dependencies.AddRange(packages);
+            dependencies.AddRange(packages);
 
-                packageBuilder.DependencySets.Add(new PackageDependencySet(null, dependencies));
-            }
+            packageBuilder.DependencySets.Add(new PackageDependencySet(null, dependencies));
+        }
 
-            var packagePath = Path.Combine(path, packageBuilder.GetFullName() + ".nupkg");
+        var packagePath = Path.Combine(path, packageBuilder.GetFullName() + ".nupkg");
 
-            using (var file = new FileStream(packagePath, FileMode.Create))
-            {
-                Log.LogMessage($"Saving file {packagePath}");
+        using (var file = new FileStream(packagePath, FileMode.Create))
+        {
+            Log.LogMessage($"Saving file {packagePath}");
 
-                packageBuilder.Save(file);
-            }
+            packageBuilder.Save(file);
         }
     }
 }
--- src/CustomBuildTasks/GitLinkTask.cs
+++ src/CustomBuildTasks/GitLinkTask.cs
@@ -1,50 +1,15 @@
-﻿using System;
-using System.Text.RegularExpressions;
-using GitLink;
-using GitLink.Providers;
+﻿using System.IO;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
-namespace CustomBuildTasks
+public class GitLinkTask : ToolTask
 {
-    public class GitLinkTask : Task
-    {
-        [Required]
-        public ITaskItem SolutionDirectory { get; set; }
+    [Required]
+    public ITaskItem SolutionDirectory { get; set; }
 
-        [Required]
-        public string TargetUrl { get; set; }
+    protected override string ToolName => "GitLink.exe";
 
-        [Required]
-        public string TargetBranch { get; set; }
+    protected override string GenerateFullPathToTool() => Path.Combine(Path.GetDirectoryName(GetType().Assembly.Location), "GitLink.exe");
 
-        public override bool Execute()
-        {
-            try
-            {
-                InnerExecute();
-            }
-            catch (System.ComponentModel.DataAnnotations.ValidationException vex)
-            {
-                foreach (var line in Regex.Split(vex.Message, Environment.NewLine))
-                    Log.LogError(line);
-            }
-            catch (Exception ex)
-            {
-                Log.LogErrorFromException(ex, true, true, null);
-            }
-
-            return !Log.HasLoggedErrors;
-        }
-
-        private void InnerExecute()
-        {
-            var context = new Context(new ProviderManager());
-            context.SolutionDirectory = SolutionDirectory.FullPath();
-            context.TargetUrl = TargetUrl;
-            context.TargetBranch = TargetBranch;
-
-            Linker.Link(context);
-        }
-    }
+    protected override string GenerateCommandLineCommands() => SolutionDirectory.FullPath();
 }
